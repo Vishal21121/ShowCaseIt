06511f7b39763583dc10039e7c3d3d3bba4d018f
Vishal21121 <vs84077000@gmail.com>
2024-07-30 09:25:39 +0530

    added: code for rendering js and css files for react and added basic react app

--- extension/src/components/Sidebar.ts
+++ extension/src/components/Sidebar.ts
@@ -4,7 +4,9 @@ export class MySidebarViewProvider implements vscode.WebviewViewProvider {
   // this is the id we have to write in view.showcaseit
   public static readonly viewType = "showcaseit.sidebarView";
 
-  constructor(private readonly _extensionUri: vscode.Uri) {}
+  constructor(private readonly _extensionUri: vscode.Uri) {
+    this._extensionUri = _extensionUri;
+  }
 
   public resolveWebviewView(
     webviewView: vscode.WebviewView,
@@ -14,21 +16,33 @@ export class MySidebarViewProvider implements vscode.WebviewViewProvider {
     webviewView.webview.options = {
       enableScripts: true,
     };
-
     webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
   }
 
   private _getHtmlForWebview(webview: vscode.Webview): string {
+    const cssPath = vscode.Uri.joinPath(
+      this._extensionUri,
+      "src/views/sidebar view/dist/index.css"
+    );
+    const cssSrc = webview.asWebviewUri(cssPath);
+    const jsPath = vscode.Uri.joinPath(
+      this._extensionUri,
+      "src/views/sidebar view/dist/index.js"
+    );
+    const jsSrc = webview.asWebviewUri(jsPath);
     return `<!DOCTYPE html>
       <html lang="en">
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>My Sidebar</title>
+        <link rel="stylesheet" href=${cssSrc}>
       </head>
       <body>
-        <h1>Hello from the Sidebar!</h1>
+        <h1>Hello from the Sidebar</h1>
         <button>Hello</button>
+        <div id="root"></div>
+        <script type="module" src=${jsSrc}></script>
       </body>
       `;
   }
