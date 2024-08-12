90b4f169122785368105b0b612717d7704cebbcd
Vishal21121 <vs84077000@gmail.com>
2024-08-12 21:18:32 +0530

    added: code for logging in the user

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -2,10 +2,11 @@
 // Import the module and reference it with the alias vscode in your code below
 import vscode from "vscode";
 import { MySidebarViewProvider } from "./components/Sidebar";
+import { Credentials } from "./githubLogin/credentials";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
   const mySidebarProvider = new MySidebarViewProvider(context.extensionUri);
   context.subscriptions.push(
     vscode.window.registerWebviewViewProvider(
@@ -14,12 +15,25 @@ export function activate(context: vscode.ExtensionContext) {
     )
   );
 
-  // context.subscriptions.push(
-  //   vscode.commands.registerCommand("showcaseit.refresh", () => {
-  //     // Implement your refresh logic here
-  //     vscode.window.showInformationMessage("Refresh command executed");
-  //   })
-  // );
+  const credentials = new Credentials();
+  await credentials.initialize(context);
+
+  const loginUser = vscode.commands.registerCommand(
+    "showcaseit.loginUser",
+    async () => {
+      const octokit = await credentials.getOctokit();
+      const userInfo = await octokit.users.getAuthenticated();
+      if (userInfo.status === 200) {
+        console.log(userInfo.data);
+      }
+
+      vscode.window.showInformationMessage(
+        `Logged into GitHub as ${userInfo.data.login}`
+      );
+    }
+  );
+
+  context.subscriptions.push(loginUser);
 }
 
 // This method is called when your extension is deactivated
